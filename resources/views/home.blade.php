a560a57a2164cbb041432a5ad0d7c85f279db581
ZIN MYO <overmidnight12@gmail.com>
2020-06-25 02:50:44 -0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+
+       @foreach($allProducts as $product)
+       <img src="{{ asset('img/bd.jpg') }}" alt="" width="300"><br>
+          {{$product->name}}<br>
+          ${{$product->price}}<br>
+
+       @endforeach
+
+
+
+
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+
+        </div>
+    </div>
+</div>
+@endsection
